a71aa2c4341f83ba894496ccb39360406c613939
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-05-16 19:24:39 +0500

    shorten functions

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -2,6 +2,10 @@ package routes
 
 import (
 	"time"
+
+	"github.com/ahmed-deftoner/url-shortener/helpers"
+	"github.com/asaskevich/govalidator"
+	"github.com/gofiber/fiber/v2"
 )
 
 type request struct {
@@ -16,4 +20,21 @@ type respomse struct {
 	Expiry        time.Duration `json:"expiry"`
 	RateRemaining int           `json:"rate_limit"`
 	RateLimitRest time.Duration `json:"rate_limit_remaining"`
+}
+
+func ShortenURL(c *fiber.Ctx) error {
+	body := new(request)
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot parse json"})
+	}
+
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "wrong url"})
+	}
+
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": "fuck u!"})
+	}
+	body.URL = helpers.EnforceHTTP(body.URL)
 }
